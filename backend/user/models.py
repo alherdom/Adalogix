9adacbdcc446937b5659019f848a62903a634d75
adrianhrb <adrian.herrera.br@gmail.com>
2024-03-11 16:05:07 +0000

    First aproximation for Employee/User model done #4

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    class EmployeeRole(models.TextChoices):
+        STORE_ADMINISTRATOR = 'SA', 'Store Administrator'
+        DELIVERY_EMPLOYEE = 'DE', 'Delivery Man'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=255)
+    phone = models.IntegerField(max_length=9)
+    role = models.CharField(max_lengt=2, choices=EmployeeRole)
