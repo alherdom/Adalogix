1ff10e3d49df201953adcd649d76ec9bd1aadc26
adrianhrb <adrian.herrera.br@gmail.com>
2024-03-11 17:29:54 +0000

    Test refactoring #4

--- backend/tests/user/test_user.py
+++ backend/tests/user/test_user.py
@@ -3,37 +3,37 @@ from django.contrib.auth.models import User
 from user.models import Employee
 
 
+@pytest.fixture
+def user():
+    return User.objects.create(username='usertest', password='usertest')
+
+
+@pytest.fixture
+def delivery_employee(
+    user, role: str = 'DE', phone: str = '666666666', department: str = 'Test department'
+):
+    return Employee.objects.create(user=user, role=role, phone=phone, department=department)
+
+
+@pytest.fixture
+def store_admin_employee(
+    user, role: str = 'SA', phone: str = '666666666', department: str = 'Test department'
+):
+    return Employee.objects.create(user=user, role=role, phone=phone, department=department)
+
+
 @pytest.mark.django_db
-def test_store_admin_employee_creation():
-    user = User(username='usertest', password='usertest')
-    employee = Employee(user=user, role='SA', phone='666666666', department='Department test')
+def test_store_admin_employee_creation(store_admin_employee, user):
+    employee = store_admin_employee
     assert employee.user == user
     assert employee.role == 'SA'
     assert employee.phone == '666666666'
-    assert employee.department == 'Department test'
-
-
-@pytest.mark.django_db
-def test_delivery_employee_creation():
-    user = User(username='usertest', password='usertest')
-    employee = Employee(user=user, role='DE', phone='666666666', department='Department test')
-    assert employee.user == user
-    assert employee.role == 'DE'
-    assert employee.phone == '666666666'
-    assert employee.department == 'Department test'
-
-
-@pytest.mark.django_db
-def test_delivery_employee_function():
-    user = User(username='usertest', password='usertest')
-    employee = Employee(user=user, role='DE', phone='666666666', department='Department test')
-    assert employee.is_delivery_employee()
-    assert not employee.is_store_admin()
-
-
-@pytest.mark.django_db
-def test_store_admin_function():
-    user = User(username='usertest', password='usertest')
-    employee = Employee(user=user, role='SA', phone='666666666', department='Department test')
-    assert not employee.is_delivery_employee()
+    assert employee.department == 'Test department'
     assert employee.is_store_admin()
+
+
+@pytest.mark.django_db
+def test_delivery_employee_creation(delivery_employee):
+    employee = delivery_employee
+    assert employee.role == 'DE'
+    assert employee.is_delivery_employee()
