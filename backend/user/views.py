95dbcb0209843619f4471806525b0b3158d7feac
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2024-03-12 17:22:24 +0100

    endpoint of user login finished #6

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpRequest, HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+@require_POST
+def user_login(request: HttpRequest) -> HttpResponse:
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        if user.groups.filter(name='admin').exists():
+            return HttpResponse('I\'m an admin')   
+        if user.groups.filter(name='truckdriver').exists():
+            return HttpResponse('I\'m a truckdriver')
+    else:
+        return HttpResponse(400)
+    
+
+
+
