db93c523d89447cef70d14360aa6b30ed6b460e4
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2024-03-19 12:50:05 +0100

    Fixed empty fields allowed in the registration form and users without a group in login #22

--- backend/user/views.py
+++ backend/user/views.py
@@ -18,6 +18,8 @@ def user_login(request: HttpRequest) -> HttpResponse:
     if user is not None:
         login(request, user)
         user_object = User.objects.get(username=user)
+        if not user_object.groups.all():
+            return HttpResponse('This user has no group',status=400)
         if user.groups.filter(name='admin').exists():
             return JsonResponse(dict(id=user_object.id, group="admin", status=200, message="Admin successfully logged in"))   
         if user.groups.filter(name='truckdriver').exists():
@@ -35,9 +37,11 @@ def user_logout(request: HttpRequest) -> HttpResponse:
 @require_POST
 def user_registration(request: HttpRequest) -> HttpResponse:
     data = json.loads(request.body)
+    username, password, email, first_name, last_name = data['username'], data['password'], data['email'], data['first_name'], data['last_name']
+    if not all([username, email, first_name, last_name, password]):
+        return HttpResponse('You must fill all the fields', status=400)
     if User.objects.filter(username = data['username']):
         return HttpResponse('This username is already in use', status=400)
-    username, password, email, first_name, last_name = data['username'], data['password'], data['email'], data['first_name'], data['last_name']
     new_user = User(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
     new_user.set_password(password)
     new_user.save()
