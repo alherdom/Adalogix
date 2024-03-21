a5582b585a27bb42cb0d646e87547d3bc80eb692
adrianhrb <adrian.herrera.br@gmail.com>
2024-03-21 16:10:10 +0000

    Product model added, many to many key left #37

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    weight = models.DecimalField(max_digits=7, decimal_places=2)
+    volume = models.DecimalField(max_digits=7, decimal_places=2)
