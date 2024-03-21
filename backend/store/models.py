1a25ae4011fc84d710901ea4b85187fa5fb45c59
adrianhrb <adrian.herrera.br@gmail.com>
2024-03-21 15:58:16 +0000

    Store model added and function to know if a product can be added #37

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    adress = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField()
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def can_add(self, amout: int) -> bool:
+        return self.capacity - amout >= 0
