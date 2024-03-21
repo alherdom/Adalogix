0b4f06d3ddc8f974e8b17ce5c8492686f6e25017
adrianhrb <adrian.herrera.br@gmail.com>
2024-03-21 16:14:53 +0000

    M2M added to product and inventory model intermediary table added #37

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from product.models import Product
+from store.models import Store
+
+
+class Inventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField()
--- backend/product/models.py
+++ backend/product/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from inventory.models import Inventory
+from store.models import Store
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -7,3 +10,4 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=9, decimal_places=2)
     weight = models.DecimalField(max_digits=7, decimal_places=2)
     volume = models.DecimalField(max_digits=7, decimal_places=2)
+    stores = models.ManyToManyField(Store, through=Inventory)
