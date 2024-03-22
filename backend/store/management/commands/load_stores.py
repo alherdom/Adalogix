05902379d076f68772a7c38cb581ca1cea3571f0
adrianhrb <adrian.herrera.br@gmail.com>
2024-03-22 20:54:47 +0000

    Minor fix on load_store address field and both new commands added to prepare_database #40

--- backend/store/management/commands/load_stores.py
+++ backend/store/management/commands/load_stores.py
@@ -14,6 +14,6 @@ class Command(BaseCommand):
             for line in f:
                 Store.objects.create(
                     name=line['name'],
-                    adress=line['address'],
+                    address=line['address'],
                     capacity=line['capacity'],
                 )
