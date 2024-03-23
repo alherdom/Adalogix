73f12a24294f77773f966cbdb4aa6563c240bc4e
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2024-03-23 22:33:53 +0000

    Created the product detail endpoint and register bug related to the roles fixed #24

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
 
 app_name = 'product'
 
 urlpatterns = [
-    path('list/', ProductListView.as_view(), name='product_list')
+    path('list/', ProductListView.as_view(), name='product_list'),
+    path('detail/<int:pk>', ProductDetailView.as_view(), name='product_detail')
 ]
