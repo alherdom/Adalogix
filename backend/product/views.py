80fdd2c6436b0dc6133a16a6b6299a7c0479b863
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2024-03-27 22:52:55 +0000

    Added authentication requirement to product views #53

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,25 +1,41 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Product
-from .serializers import ProductListSerializer, ProductDetailSerializer
+from .serializers import ProductDetailSerializer, ProductListSerializer
+
 
 class ProductListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
-    
+
+
 class ProductDetailView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductDetailSerializer
+
 
 class ProductCreateView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductDetailSerializer
+
 
 class ProductDeleteView(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductDetailSerializer
+
 
 class ProductUpdateView(UpdateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductDetailSerializer
-
