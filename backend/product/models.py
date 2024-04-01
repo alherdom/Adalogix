99ae6fe24806aa16fd30fcd5587524486480d943
adrianhrb <adrian.herrera.br@gmail.com>
2024-04-01 23:57:22 +0100

    New field added to product #66

--- backend/product/models.py
+++ backend/product/models.py
@@ -6,6 +6,7 @@ from store.models import Store
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    category = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     weight = models.DecimalField(max_digits=7, decimal_places=2)
     volume = models.DecimalField(max_digits=7, decimal_places=2)
