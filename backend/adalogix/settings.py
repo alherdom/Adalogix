4b89e654efe4b834134d5787b441147681b2899f
adrianhrb <adrian.herrera.br@gmail.com>
2024-04-02 10:43:07 +0100

    New product list endpoint added to retrieve the quantity and category added to model

--- backend/adalogix/settings.py
+++ backend/adalogix/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from prettyconf import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,9 +85,9 @@ WSGI_APPLICATION = 'adalogix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'adalogix',
-        'USER': 'adalogix',
-        'PASSWORD': 'adalogix',
+        'NAME': config('POSTGRES_USER'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
--- backend/product/views.py
+++ backend/product/views.py
@@ -1,4 +1,5 @@
 import json
+
 from django.http import JsonResponse
 from rest_framework.generics import (
     CreateAPIView,
@@ -10,9 +11,9 @@ from rest_framework.generics import (
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-
-from .models import Product
+from .models import Inventory, Product
 from .serializers import ProductDetailSerializer, ProductListSerializer
+
 
 class ProductMultipleDelete(APIView):
     def delete(self, request):
@@ -22,10 +23,30 @@ class ProductMultipleDelete(APIView):
             Product.objects.get(id=product_id).delete()
         return JsonResponse({'deleted': 'true'})
 
+
 class ProductListView(ListAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
+
+
+class ProductList(APIView):
+    def get(self, request):
+        products_to_return = []
+        products = Product.objects.all()
+        for product in products:
+            quantity = Inventory.objects.filter(product=product).count()
+            products_to_return.append(
+                {
+                    'id': product.id,
+                    'description': product.description,
+                    'quantity': quantity,
+                    'pirce': product.price,
+                    'weight': product.weight,
+                    'volume': product.volume,
+                }
+            )
+        return JsonResponse(products_to_return, safe=False)
 
 
 class ProductDetailView(RetrieveAPIView):
