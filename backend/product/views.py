94732b6e8e2a8daadee995ff22af19332b8ba681
adrianhrb <adrian.herrera.br@gmail.com>
2024-04-05 11:34:11 +0100

    Endpoint created, not tested yet

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,18 +1,15 @@
+import csv
 import json
 
 from django.http import JsonResponse
-from rest_framework.generics import (
-    CreateAPIView,
-    DestroyAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-    UpdateAPIView,
-)
+from rest_framework.generics import CreateAPIView, DestroyAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
+from store.models import Store
+
 from .models import Inventory, Product
-from .serializers import ProductDetailSerializer, ProductListSerializer
+from .serializers import ProductDetailSerializer
 
 
 class ProductMultipleDelete(APIView):
@@ -22,12 +19,6 @@ class ProductMultipleDelete(APIView):
         for product_id in product_ids:
             Product.objects.get(id=product_id).delete()
         return JsonResponse({'status': 200})
-
-
-class ProductListView(ListAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = Product.objects.all()
-    serializer_class = ProductListSerializer
 
 
 class ProductList(APIView):
@@ -48,6 +39,30 @@ class ProductList(APIView):
                 }
             )
         return JsonResponse(products_to_return, safe=False)
+
+
+class ProductCreateCsv(APIView):
+    def post(self, request):
+        if file := request.FILES.get('products'):
+            decoded_file = file.read().decode('utf-8').splitlines()
+            reader = csv.DictReader(decoded_file)
+            general_store = Store.objects.get(id=0)
+            for row in reader:
+                name, description, price, category, weight, volume, quantity = (
+                    row['name'],
+                    row['description'],
+                    row['price'],
+                    row['category'],
+                    row['weight'],
+                    row['volume'],
+                    row['quantity'],
+                )
+                new_product = Product.objects.create(
+                    name, description, price, category, weight, volume
+                )
+                Inventory.objects.create(product=new_product, store=general_store, stock=quantity)
+            return JsonResponse({'status', 200})
+        return JsonResponse({'status', 400})
 
 
 class ProductDetailView(RetrieveAPIView):
