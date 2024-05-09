a4ff48eca8fcdbfbafaac38bd7f8cdf7410fdb6c
adrianherrerabr <adrian.herrera@sanctions.io>
2024-05-09 23:14:23 +0100

    Static root added

--- backend/adalogix/settings.py
+++ backend/adalogix/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from prettyconf import config
 from pathlib import Path
 
 from prettyconf import config
@@ -130,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
