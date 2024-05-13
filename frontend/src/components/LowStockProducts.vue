dde75ffd785f9943e4ba26bc0c2600663d94ec56
adrianherrerabr <adrian.herrera@sanctions.io>
2024-05-13 22:27:39 +0100

    Some changes

---
+++ frontend/src/components/LowStockProducts.vue
@@ -0,0 +1,447 @@
+<template>
+  <q-table
+    flat
+    class="my-sticky-header-table"
+    title="Inventory Table"
+    :rows="displayedItems"
+    :columns="columns"
+    :loading="loading"
+    row-key="id"
+    v-model:selected="selectedRows"
+    selection="multiple"
+    :pagination="{ rowsPerPage: 20 }"
+  >
+    <template v-slot:top>
+      <q-btn
+        push
+        size="5px"
+        class="q-ml-sm q-mt-sm"
+        color="white"
+        text-color="black"
+        :disable="loading"
+        icon="delete"
+        @click="deleteItem"
+      >
+        <q-tooltip
+          anchor="bottom middle"
+          transition-show="scale"
+          transition-hide="scale"
+        >
+          Delete Items
+        </q-tooltip>
+      </q-btn>
+      <q-btn
+        push
+        size="5px"
+        class="q-ml-sm q-mt-sm"
+        color="white"
+        text-color="black"
+        :disable="loading"
+        icon="upload"
+        @click="openUploaderDialog"
+        ><q-tooltip
+          anchor="bottom middle"
+          transition-show="scale"
+          transition-hide="scale"
+        >
+          Upload Products
+        </q-tooltip>
+      </q-btn>
+      <q-btn
+        push
+        size="5px"
+        class="q-ml-sm q-mt-sm"
+        color="white"
+        text-color="black"
+        :disable="loading"
+        icon="download"
+        @click="exportTable"
+      >
+        <q-tooltip
+          anchor="bottom middle"
+          transition-show="scale"
+          transition-hide="scale"
+        >
+          Export Table
+        </q-tooltip>
+      </q-btn>
+      <q-space />
+      <q-input
+        dense
+        filled
+        class="q-mr-sm q-mt-sm"
+        placeholder="Search Product..."
+        debounce="300"
+        color="primary"
+        v-model="filter"
+      >
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+        <q-tooltip
+          anchor="bottom middle"
+          transition-show="scale"
+          transition-hide="scale"
+        >
+          By name, description or category
+        </q-tooltip>
+      </q-input>
+    </template>
+    <template v-slot:body-cell-actions="props">
+      <q-td>
+        <q-btn
+          flat
+          size="sm"
+          color="primary"
+          icon="edit"
+          @click="editItem(props.row)"
+        />
+        <q-btn
+          flat
+          size="sm"
+          color="primary"
+          icon="visibility"
+          @click="showProductDetail(props.row)"
+        />
+      </q-td>
+    </template>
+  </q-table>
+
+  <!-- Dialogs-->
+  <q-dialog v-model="uploaderDialog">
+    <q-uploader
+      text-color="black"
+      ref="uploader"
+      url="https://backend.adalogix.es/product/upload/"
+      label="Select a CSV file to upload"
+      single-file
+      accept=".csv"
+      @uploaded="handleUpload"
+    >
+    </q-uploader>
+  </q-dialog>
+  <q-dialog v-model="editItemForm">
+    <EditItemForm :item="itemData" @closeEditForm="closeEditItemForm" />
+  </q-dialog>
+  <q-dialog v-model="productDetail">
+    <q-table
+      class="product-detail-table"
+      flat
+      :rows="[productDetailRow]"
+      :columns="columns.slice(0, -1)"
+      :loading="loading"
+      row-key="id"
+      hide-bottom
+    >
+      <template v-slot:bottom-row>
+        <q-btn
+          class="close-product-detail-btn"
+          flat
+          dense
+          size="sm"
+          icon="close"
+          @click="productDetail = false"
+        />
+      </template>
+    </q-table>
+  </q-dialog>
+</template>
+
+<script setup>
+import Swal from "sweetalert2";
+import { getRequest, deleteRequest } from "../utils/common";
+import { ref, onMounted, computed } from "vue";
+import { exportFile, useQuasar } from "quasar";
+import EditItemForm from "./EditItemForm.vue";
+
+const productDetailRows = ref([
+  {
+    id: 1,
+    name: "Product 1",
+    description: "Product 1 Description",
+    quantity: 10,
+    category: "Category 1",
+    price: 100,
+    weight: 10,
+    volume: 10,
+  },
+]);
+
+const uploaderDialog = ref(false);
+const openUploaderDialog = () => {
+  uploaderDialog.value = true;
+};
+
+const handleUpload = () => {
+  uploaderDialog.value = false;
+  Swal.fire({
+    icon: "success",
+    title: "Items loaded successfully!",
+  });
+  fetchData();
+};
+
+function wrapCsvValue(val, formatFn, row) {
+  let formatted = formatFn !== void 0 ? formatFn(val, row) : val;
+  formatted =
+    formatted === void 0 || formatted === null ? "" : String(formatted);
+  formatted = formatted.split('"').join('""');
+  return `"${formatted}"`;
+}
+
+function parseStore(store) {
+  return JSON.parse(store);
+}
+
+const closeDialog = (status) => {
+  editProductForm.value = status;
+  fetchData();
+};
+
+const productData = ref({});
+
+const editProductForm = ref(false);
+
+const editProduct = (productProps) => {
+  productData.value = productProps;
+  editProductForm.value = true;
+};
+
+function openEditDialog(data) {}
+
+const extendedRowColumns = [
+  {
+    name: "id",
+    required: true,
+    label: "Id",
+    align: "left",
+    field: "id",
+    sortable: true,
+  },
+  {
+    name: "name",
+    required: true,
+    label: "Store",
+    align: "left",
+    field: "name",
+    sortable: true,
+  },
+  {
+    name: "quantity",
+    required: true,
+    label: "Quantity",
+    align: "left",
+    field: "quantity",
+    sortable: true,
+  },
+  {
+    name: "address",
+    required: true,
+    label: "Address",
+    align: "left",
+    field: "address",
+    sortable: true,
+  },
+];
+
+const $q = useQuasar();
+const exportTable = () => {
+  let rowsToExport =
+    selectedRows.value.length > 0 ? selectedRows.value : items.value;
+  const columnsToExport = columns.slice(0, -1);
+  const content = [columnsToExport.map((col) => wrapCsvValue(col.label))]
+    .concat(
+      rowsToExport.map((row) =>
+        columnsToExport
+          .map((col) =>
+            wrapCsvValue(
+              typeof col.field === "function"
+                ? col.field(row)
+                : row[col.field === void 0 ? col.name : col.field],
+              col.format,
+              row
+            )
+          )
+          .join(",")
+      )
+    )
+    .join("\r\n");
+
+  const status = exportFile("inventory-table.csv", content, "text/csv");
+
+  if (status !== true) {
+    $q.notify({
+      message: "Browser denied file download...",
+      color: "negative",
+      icon: "warning",
+    });
+  }
+};
+
+const columns = [
+  {
+    name: "id",
+    required: true,
+    label: "Id",
+    align: "left",
+    field: "id",
+    sortable: true,
+  },
+  {
+    name: "name",
+    required: true,
+    label: "Name",
+    align: "left",
+    field: "name",
+    sortable: true,
+  },
+  {
+    name: "description",
+    required: true,
+    label: "Description",
+    align: "left",
+    field: "description",
+    sortable: true,
+  },
+  {
+    name: "quantity",
+    required: true,
+    label: "Quantity",
+    align: "left",
+    field: "quantity",
+    sortable: true,
+  },
+  {
+    name: "category",
+    required: true,
+    label: "Category",
+    align: "left",
+    field: "category",
+    sortable: true,
+  },
+  {
+    name: "price",
+    required: true,
+    label: "Price",
+    align: "left",
+    field: "price",
+    sortable: true,
+  },
+  {
+    name: "weight",
+    required: true,
+    label: "Weight",
+    align: "left",
+    field: "weight",
+    sortable: true,
+  },
+  {
+    name: "volume",
+    required: true,
+    label: "Volume",
+    align: "left",
+    field: "volume",
+    sortable: true,
+  },
+  { name: "actions", label: "Actions", align: "left", field: "actions" },
+];
+
+const itemData = ref({});
+const loading = ref(false);
+const selectedRows = ref([]);
+const items = ref([]);
+const filter = ref("");
+const editItemForm = ref(false);
+const productDetail = ref(false);
+const productDetailRow = ref({});
+
+const fetchData = async () => {
+  try {
+    loading.value = true;
+    const url = "https://backend.adalogix.es/product/list/lowstock/";
+    const response = await getRequest(url);
+    items.value = response.map((item) => ({ ...item, expanded: false }));
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong while fetching data!",
+    });
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(fetchData);
+
+const deleteItem = async () => {
+  if (selectedRows.value.length === 0) {
+    Swal.fire({
+      title: "No items selected",
+      text: "Please select items to delete",
+      icon: "error",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    return;
+  }
+  Swal.fire({
+    title: "Are you sure you want to delete?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+    cancelButtonText: "No",
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      const requestData = {
+        product_ids: selectedRows.value.map((item) => item.id),
+      };
+      const url = "https://backend.adalogix.es/product/delete/multiple/";
+      try {
+        const response = await deleteRequest(requestData, url);
+        if (response.status === 200) {
+          selectedRows.value = [];
+          fetchData();
+        }
+      } catch (error) {
+        console.error("Error deleting items:", error);
+      }
+    }
+  });
+};
+
+const editItem = (itemProps) => {
+  itemData.value = itemProps;
+  editItemForm.value = true;
+};
+
+const closeEditItemForm = (value) => {
+  editItemForm.value = value;
+  fetchData();
+};
+
+const showProductDetail = (itemProps) => {
+  console.log(itemProps);
+  productDetailRow.value = itemProps;
+  productDetail.value = true;
+};
+
+const displayedItems = computed(() => {
+  return items.value.filter((item) => {
+    if (!filter.value) return true;
+    const search = filter.value.toLowerCase();
+    return (
+      item.name.toLowerCase().includes(search) ||
+      item.description.toLowerCase().includes(search) ||
+      item.category.toLowerCase().includes(search)
+    );
+  });
+});
+</script>
+
+<style scoped>
+.my-sticky-header-table {
+  height: 50%;
+}
+</style>
--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="logo">
-    <img src="../assets/box3d-three-points.svg" alt="Adalogix logo">
-    <h2 class="title">Adalogix</h2>
+    <h2>Low Stock Products!</h2>
+    <LowStock class="prueba" />
   </div>
 </template>
 
 <script setup>
+import LowStock from "../components/LowStockProducts.vue";
 </script>
 
 <style scoped>
 .logo {
   display: flex;
   justify-content: center;
-  align-items: center;
+  flex-direction: column;
   height: 80vh;
 }
 
 img {
   width: 250px;
-  ;
 }
 
 .title {
