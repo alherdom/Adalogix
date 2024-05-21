826ba9fcf7aa2bc671522d8d580123fe7285f6cd
alherdom <alherdom@outlook.com>
2024-05-21 21:25:11 +0100

    WIP2

--- frontend/src/components/EditUserForm.vue
+++ frontend/src/components/EditUserForm.vue
@@ -96,6 +96,7 @@ const sendData = async () => {
     };
     console.log(requestData);
     // const url = `http://localhost:8000/user/update/${id.value}/`;
+    // HOLA HOLA
     const url = `https://backend.adalogix.es/user/update/${id.value}/`;
     const response = await patchRequest(requestData, url);
     if (response.status === 200) {
